f9b2e8fd4c40cad42ab8681189cf2856633adbfa
Dewayne Wagner <dewayne@rockyspring.ca>
2020-09-29 11:32:32 -0600

    Reconfigured structure of project, to include Unit Test projects inside of PokerJudge.

---
+++ UnitTests/HandIdentifierTests.cs
@@ -0,0 +1,56 @@
+﻿
+using Poker_Judge.PokerEngine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static Poker_Judge.PokerEngine.Deck;
+using HT = Poker_Judge.PokerEngine.HandRanker.HandTypes;
+
+namespace UnitTests
+{
+    class HandIdentifierTests
+    {
+        private List<Hand> _testHands = new List<Hand>()
+        {
+            new Hand("AH KH QH JH 8S", "10H 7D"), // Royal Flush
+            new Hand("10S 8S 7D KH 2S", "3S 5S"), // Flush
+            new Hand("3S 4S 5S 6S 8D", "7S 9C"), // Straight Flush
+            new Hand("3S 4C 5D KD QS", "6D 7D"), // Straight
+            new Hand("5H 6H 9H 2D QC", "10H KH"), // Flush
+            new Hand("4D 8C 6D 8D KH", "QD 2H"), // Pair
+            new Hand("5S 5D 5H 8C QD", "2C AH"), // Three of a kind
+            new Hand("2H 4D 6C 9S KH", "KC KD"), // Three of a kind
+            new Hand("JH JC JS 9D 2S", "JD 4H"), // Four of a kind
+            new Hand("4S 4D 2S KH JD", "4H 4C"), // Four of a kind
+            new Hand("5S 5H 5D 2C QD", "JD JC"), // Full House
+            new Hand("7D 7C QC AD 2S", "7H AC"), // Full House
+            new Hand("8D 9C QH 2S 5D", "8H 10D"), // Pair
+            new Hand("8D 9C 9H 2S 5D", "8H 10D"), // Two Pair
+        };
+
+        private List<bool> _containsFlushKey = new List<bool>() { true, true, true, false, true, false, false, false, false, false, false, false, false, false };
+        private List<int> _highCardKey = new List<int>() { 12, 11, 7, 11, 11, 11, 12, 11, 9, 11, 10, 12, 10, 8 };
+        private List<bool> _containsStraightKey = new List<bool>() { true, false, true, true, false, false, false, false, false, false, false, false, false, false };
+        private List<bool> _containsThreeOfAKindKey = new List<bool>() { false, false, false, false, false, false, true, true, false, false, true, true, false, false };
+        private List<bool> _containsFourOfAKindKey = new List<bool>() { false, false, false, false, false, false, false, false, true, true, false, false, false, false };
+        private List<bool> _containsFullHouseKey = new List<bool>() { false, false, false, false, false, false, false, false, false, false, true, true, false, false };
+        private List<bool> _containsPairKey = new List<bool>() { false, false, false, false, false, true, false, false, false, false, true, true, true, true };
+        private List<int> _rankingKey = new List<int>() { (int)HT.RoyalFlush, (int)HT.Flush, (int)HT.StraightFlush, (int)HT.Straight, (int)HT.Flush, (int)HT.Pair, (int)HT.ThreeOfAKind,
+                                                            (int)HT.ThreeOfAKind, (int)HT.FourOfAKind, (int)HT.FourOfAKind, (int)HT.FullHouse, (int)HT.FullHouse, (int)HT.Pair, (int)HT.TwoPair };
+        private List<bool> _containsTwoPairKey = new List<bool>() { false, false, false, false, false, false, false, false, false, false, false, false, false, true };
+
+        public List<Hand> TestHands => _testHands;
+        public List<bool> ContainsFlushKey => _containsFlushKey;
+        public List<int> HightCardKey => _highCardKey;
+        public List<bool> ContainsStraightKey => _containsStraightKey;
+        public List<bool> ContainsThreeOfAKindKey => _containsThreeOfAKindKey;
+        public List<bool> ContainsFourOfAKindKey => _containsFourOfAKindKey;
+        public List<bool> ContainsFullHouseKey => _containsFullHouseKey;
+        public List<bool> ContainsPairKey => _containsPairKey;
+        public List<bool> ContainsTwoPairKey => _containsTwoPairKey;
+        public List<int> RankingKey => _rankingKey;
+        public int NumberOfTestHands => TestHands.Count;
+
+
+    }
+}
---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,253 @@
+using NUnit.Framework;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Security;
+using static Poker_Judge.PokerEngine.Deck;
+using Poker_Judge.PokerEngine;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using Poker_Judge.PokerMain;
+
+namespace UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup() { }
+
+        [Test]
+        public void parse_aceofhearts_aceofheartscard()
+        {
+            int value = 12;
+            Suits suit = Suits.H;
+
+            Card card = new Card(value, suit);
+
+            Assert.That(card.Value, Is.EqualTo(12));
+            Assert.That(card.Suit, Is.EqualTo(Suits.H));
+            Assert.That(card.FaceValue, Is.EqualTo("AH"));
+        }
+        [Test]
+        public void parse_twoofdiamonds_twoofdiamondscard()
+        {
+            int value = 0;
+            Suits suit = Suits.D;
+
+            Card card = new Card(value, suit);
+
+            Assert.That(card.Value, Is.EqualTo(0));
+            Assert.That(card.Suit, Is.EqualTo(Suits.D));
+            Assert.That(card.FaceValue, Is.EqualTo("2D"));
+        }
+        [Test]
+        public void count_deckis52cards_52cards()
+        {
+            Deck deck = new Deck();
+
+            Assert.That(deck.Count, Is.EqualTo(52));
+        }
+        [Test]
+        public void parse_jackofclubs_jackofclubscard()
+        {
+            string fv = "JC";
+            Card card = new Card(fv);
+
+            Assert.That(card.FaceValue, Is.EqualTo("JC"));
+            Assert.That(card.Suit, Is.EqualTo(Suits.C));
+            Assert.That(card.Value, Is.EqualTo(9));
+        }
+        [Test]
+        public void create_sevenfacevalues_hand()
+        {
+            List<string> faceValues = new List<string>() { "AH", "KH", "QH", "JH", "10H", "9H", "8H" };
+            string communitycards = "AH KH QH JH 10H";
+            string holeCards = "9H 8H";
+            
+            int numberOfCards = faceValues.Count;
+
+            Hand hand = new Hand(communitycards, holeCards);
+
+            for (int c = 0; c < numberOfCards; c++)
+            {
+                Assert.That(hand[c].FaceValue, Is.EqualTo(faceValues[c]));
+            }
+        }
+        [Test]
+        public void create_sevenfacevalues_hand2()
+        {
+            List<string> faceValues = new List<string>() { "3S", "4C", "5D", "KD", "QS", "6D", "7D" };
+            List<int> values = new List<int>() { 1, 2, 3, 11, 10, 4, 5 };
+            string communityCards = "3S 4C 5D KD QS";
+            string holeCards = "6D 7D";
+
+            int numberOfCards = faceValues.Count;
+
+            Hand hand = new Hand(communityCards, holeCards);
+
+            for (int c = 0; c < numberOfCards; c++)
+            {
+                Assert.That(hand[c].FaceValue, Is.EqualTo(faceValues[c]));
+                Assert.That(hand[c].Value, Is.EqualTo(values[c]));
+            }
+        }
+        [Test]
+        public void handranker_testhands_isflush()
+        {
+            HandIdentifierTests hit = new HandIdentifierTests();
+
+            for (int i = 0; i < hit.NumberOfTestHands; i++)
+            {
+                Hand hand = hit.TestHands[i];
+                HandRanker hr = new HandRanker(hand);
+                Assert.That(hr.ContainsFlush, Is.EqualTo(hit.ContainsFlushKey[i]));
+            }
+        }
+        [Test]
+        public void handranker_testhands_isstraight()
+        {
+            HandIdentifierTests hit = new HandIdentifierTests();
+
+            for (int i = 0; i < hit.NumberOfTestHands; i++)
+            {
+                Hand hand = hit.TestHands[i];
+                HandRanker hr = new HandRanker(hand);
+                Assert.That(hr.ContainsStraight, Is.EqualTo(hit.ContainsStraightKey[i]));
+            }
+        }
+        [Test]
+        public void handranker_testhands_highcard()
+        {
+            HandIdentifierTests hit = new HandIdentifierTests();
+
+            for (int i = 0; i < hit.NumberOfTestHands; i++)
+            {
+                Hand hand = hit.TestHands[i];
+                HandRanker hr = new HandRanker(hand);
+                Assert.That(hr.HighCard, Is.EqualTo(hit.HightCardKey[i]));
+            }
+        }
+        [Test]
+        public void handranker_testhands_threeofakind()
+        {
+            HandIdentifierTests hit = new HandIdentifierTests();
+
+            for (int h = 0; h < hit.NumberOfTestHands; h++)
+            {
+                Hand hand = hit.TestHands[h];
+                HandRanker hr = new HandRanker(hand);
+                Assert.That(hr.ContainsThreeOfAKind, Is.EqualTo(hit.ContainsThreeOfAKindKey[h]));
+            }
+        }
+        [Test]
+        public void handranker_testhands_fourofakind()
+        {
+            HandIdentifierTests hit = new HandIdentifierTests();
+
+            for (int h = 0; h < hit.NumberOfTestHands; h++)
+            {
+                Hand hand = hit.TestHands[h];
+                HandRanker hr = new HandRanker(hand);
+                Assert.That(hr.ContainsFourOfAKind, Is.EqualTo(hit.ContainsFourOfAKindKey[h]));
+            }
+        }
+        [Test]
+        public void handranker_testhands_fullhouse()
+        {
+            HandIdentifierTests hit = new HandIdentifierTests();
+
+            for (int h = 0; h < hit.NumberOfTestHands; h++)
+            {
+                Hand hand = hit.TestHands[h];
+                HandRanker hr = new HandRanker(hand);
+                Assert.That(hr.ContainsFullHouse, Is.EqualTo(hit.ContainsFullHouseKey[h]));
+            }
+        }
+        [Test]
+        public void handranker_testhands_pair()
+        {
+            HandIdentifierTests hit = new HandIdentifierTests();
+
+            for (int h = 0; h < hit.NumberOfTestHands; h++)
+            {
+                Hand hand = hit.TestHands[h];
+                HandRanker hr = new HandRanker(hand);
+                Assert.That(hr.ContainsPair, Is.EqualTo(hit.ContainsPairKey[h]));
+            }
+        }
+        [Test]
+        public void handranker_testhands_twopair()
+        {
+            HandIdentifierTests hit = new HandIdentifierTests();
+
+            for (int h = 0; h < hit.NumberOfTestHands; h++)
+            {
+                Hand hand = hit.TestHands[h];
+                HandRanker hr = new HandRanker(hand);
+                Assert.That(hr.ContainsTwoPair, Is.EqualTo(hit.ContainsTwoPairKey[h]));
+            }
+        }
+        [Test]
+        public void handranker_testhands_ranking()
+        {
+            HandIdentifierTests hit = new HandIdentifierTests();
+
+            for (int h = 0; h < hit.NumberOfTestHands; h++)
+            {
+                Hand hand = hit.TestHands[h];
+                HandRanker hr = new HandRanker(hand);
+                Assert.That(hr.Ranking, Is.EqualTo(hit.RankingKey[h]));
+            }
+        }
+        [Test]
+        public void pokerjudge_choosewinner_player1()
+        {
+            string communityCards = "4H 5H 6H QD 9C";
+            List<string> playerCards = new List<string>()
+            {
+                "7H 3H",
+                "KC 2D",
+                "AS 3D",
+            };
+
+            string correctWinner = "Player1";
+
+            PokerJudge pj = new PokerJudge();
+            string chosenWinner = pj.GetWinner(communityCards, playerCards);
+            Assert.That(chosenWinner, Is.EqualTo(correctWinner));                
+        }
+        [Test]
+        public void pokerjudge_choosewinner_player2()
+        {
+            string communityCards = "2H 4D 6C 9S KH";
+            List<string> playerCards = new List<string>()
+            {
+                "7H 3H",
+                "KD KC",
+                "AS 3D",
+            };
+
+            string correctWinner = "Player2";
+
+            PokerJudge pj = new PokerJudge();
+            string chosenWinner = pj.GetWinner(communityCards, playerCards);
+            Assert.That(chosenWinner, Is.EqualTo(correctWinner));
+        }
+        [Test]
+        public void pokerjudge_choosewinner_player3()
+        {
+            string communityCards = "8D 9C 9H 2S 5D";
+            List<string> playerCards = new List<string>()
+            {
+                "7H 3H",
+                "8S KC",
+                "AS 8C",
+            };
+
+            string correctWinner = "Player3";
+
+            PokerJudge pj = new PokerJudge();
+            string chosenWinner = pj.GetWinner(communityCards, playerCards);
+            Assert.That(chosenWinner, Is.EqualTo(correctWinner));
+        }
+    }
+}
