70a482deed1647f55ef245e17eb268693837e762
Dewayne Wagner <dewayne@rockyspring.ca>
2020-09-29 15:57:34 -0600

    Updated HandRank class, to clean-up method calls.

--- PokerJudge/PokerEngine/HandRank.cs
+++ PokerJudge/PokerEngine/HandRank.cs
@@ -15,12 +15,27 @@ namespace Poker_Judge.PokerEngine
             FourOfAKind, StraightFlush, RoyalFlush, 
         }
 
-        private Hand _hand;
-        public HandRank(Hand hand)
+        private bool _containsFlush;
+        private bool _containsStraight;
+        private List<int> _distinctValues;
+        private bool _containsThreeOfAKind;
+        private bool _containsPair;
+
+        public int PlayerNumber { get; set; }
+        public HandTypes Ranking { get; set; }
+        public int HighCard { get; set; }
+
+        public void Run(Hand hand)
         {
-            _hand = hand;
+            PlayerNumber = hand.PlayerNumber;
+            HighCard = GetHighCard(hand);
+            _distinctValues = GetListOfDistinctValues(hand);
+            _containsFlush = CheckIfContainsFlush(hand);
+            _containsStraight = CheckIfContainsStraight(hand);
+            _containsThreeOfAKind = CheckIfContainsThreeOfAKind(hand);
+            _containsPair = CheckIfContainsPair(hand);
+            Ranking = GetHandType(hand);
         }
-        public int PlayerNumber => _hand.PlayerNumber;
 
         private bool CheckIfContainsFlush(Hand hand)
         {
@@ -39,141 +54,124 @@ namespace Poker_Judge.PokerEngine
             return false;
         }
 
-        public bool ContainsStraight
+        private bool CheckIfContainsStraight(Hand hand)
         {
-            get
-            {
-                List<int> orderedValues = _hand.OrderByDescending(c => c.Value)
+            List<int> orderedValues = hand.OrderByDescending(c => c.Value)
                                                 .Select(c => c.Value)
                                                 .ToList();
 
-                List<int> differentialList = getDifferentialList();
+            List<int> differentialList = getDifferentialList();
 
-                int counter = 0;
-                bool isPossibleStraight = false;
+            int counter = 0;
+            bool isPossibleStraight = false;
 
-                foreach (int diff in differentialList)
-                {
-                    if(diff == 1 && isPossibleStraight) { counter++; }
-                    else if(diff == 1 && !isPossibleStraight)
-                    {
-                        counter++;
-                        isPossibleStraight = true;
-                    }
-                    else
-                    {
-                        counter = 0;
-                        isPossibleStraight = false;
-                    }
-                    if(counter == 4) { return true; }
-                }
-                return false;
-
-                List<int> getDifferentialList()
-                {
-                    List<int> diffList = new List<int>();
-                    for (int i = 0; i < orderedValues.Count - 1; i++)
-                    {
-                        int diff = orderedValues[i] - orderedValues[i + 1];
-                        if (diff != 0) { diffList.Add(diff); }
-                    }
-                    return diffList;
-                }
-            }
-        }
-        private List<int> _distinctValues => _hand.Select(v => v.Value)
-                                            .Distinct()
-                                            .ToList();
-
-        public bool ContainsThreeOfAKind
-        {
-            get
+            foreach (int diff in differentialList)
             {
-                foreach (int v in _distinctValues)
+                if (diff == 1 && isPossibleStraight) { counter++; }
+                else if (diff == 1 && !isPossibleStraight)
                 {
-                    if (_hand.Where(c => c.Value == v).Count() == 3) { return true; }
+                    counter++;
+                    isPossibleStraight = true;
                 }
-                return false;
+                else
+                {
+                    counter = 0;
+                    isPossibleStraight = false;
+                }
+                if (counter == 4) { return true; }
             }
-        }
+            return false;
 
-        public bool ContainsFourOfAKind
-        {
-            get
+            List<int> getDifferentialList()
             {
-                foreach (int v in _distinctValues)
+                List<int> diffList = new List<int>();
+                for (int i = 0; i < orderedValues.Count - 1; i++)
                 {
-                    if (_hand.Where(c => c.Value == v).Count() == 4) { return true; }
+                    int diff = orderedValues[i] - orderedValues[i + 1];
+                    if (diff != 0) { diffList.Add(diff); }
                 }
-                return false;
+                return diffList;
             }
         }
 
-        public bool ContainsFullHouse
+        private List<int> GetListOfDistinctValues(Hand hand) => hand.Select(v => v.Value)
+                                                                    .Distinct()
+                                                                    .ToList();
+
+        private bool CheckIfContainsThreeOfAKind(Hand hand)
         {
-            get
+            foreach (int v in _distinctValues)
             {
-                if (ContainsThreeOfAKind)
-                {
-                    foreach (int value in _distinctValues)
-                    {
-                        if(_hand.Where(v => v.Value == value).Count() == 2) { return true; }
-                    }                    
-                }
-                return false;
+                if (hand.Where(c => c.Value == v).Count() == 3) { return true; }
             }
+            return false;
         }
 
-        public bool ContainsPair
+        private bool ContainsFourOfAKind(Hand hand)
         {
-            get
+            foreach (int v in _distinctValues)
+            {
+                if (hand.Where(c => c.Value == v).Count() == 4) { return true; }
+            }
+            return false;
+        }
+
+        private bool ContainsFullHouse(Hand hand)
+        {
+            if (_containsThreeOfAKind)
             {
                 foreach (int value in _distinctValues)
                 {
-                    if (_hand.Where(v => v.Value == value).Count() == 2) { return true; }
+                    if (hand.Where(v => v.Value == value).Count() == 2) { return true; }
                 }
-                return false;
             }
+            return false;
         }
 
-        public bool ContainsTwoPair
+        private bool CheckIfContainsPair(Hand hand)
         {
-            get
+            foreach (int value in _distinctValues)
             {
-                if (ContainsPair)
+                if (hand.Where(v => v.Value == value).Count() == 2) { return true; }
+            }
+            return false;
+        }
+
+        private bool ContainsTwoPair(Hand hand)
+        {
+            if (_containsPair)
+            {
+                int numberOfPairs = 0;
+
+                foreach (int value in _distinctValues)
                 {
-                    int numberOfPairs = 0;
-
-                    foreach (int value in _distinctValues)
-                    {
-                        if(_hand.Where(v => v.Value == value).Count() == 2) { numberOfPairs++; }
-                    }
-                    if(numberOfPairs == 2) { return true; }
+                    if(hand.Where(v => v.Value == value).Count() == 2) { numberOfPairs++; }
                 }
-                return false;
+                    
+                if (numberOfPairs == 2) { return true; }
             }
+            return false;
         }
-
-        public int Ranking
+        private int GetHighCard(Hand hand)
         {
-            get
-            {
-                if(ContainsStraight && CheckIfContainsFlush(_hand) && HighCard == 12) { return (int)HandTypes.RoyalFlush; }
-                if(ContainsStraight && CheckIfContainsFlush(_hand)) { return (int)HandTypes.StraightFlush; }
-                if(ContainsFourOfAKind) { return (int)HandTypes.FourOfAKind; }
-                if(ContainsFullHouse) { return (int)HandTypes.FullHouse; }
-                if(CheckIfContainsFlush(_hand)) { return (int)HandTypes.Flush; }
-                if(ContainsStraight) { return (int)HandTypes.Straight; }
-                if(ContainsThreeOfAKind) { return (int)HandTypes.ThreeOfAKind; }
-                if(ContainsTwoPair) { return (int)HandTypes.TwoPair; }
-                if(ContainsPair) { return (int)HandTypes.Pair; }
-
-                return (int)HandTypes.HighCard;
-            }
-        }
-
-        public int HighCard => _hand.OrderByDescending(c => c.Value)
+            return hand.OrderByDescending(c => c.Value)
                                     .Select(c => c.Value)
                                     .First();
+        }
+
+        private HandTypes GetHandType(Hand hand)
+        {
+            if (_containsStraight && _containsFlush && HighCard == 12) { return HandTypes.RoyalFlush; }
+            if (_containsStraight && _containsFlush) { return HandTypes.StraightFlush; }
+            if (ContainsFourOfAKind(hand)) { return HandTypes.FourOfAKind; }
+            if (ContainsFullHouse(hand)) { return HandTypes.FullHouse; }
+            if (CheckIfContainsFlush(hand)) { return HandTypes.Flush; }
+            if (_containsStraight) { return HandTypes.Straight; }
+            if (_containsThreeOfAKind) { return HandTypes.ThreeOfAKind; }
+            if (ContainsTwoPair(hand)) { return HandTypes.TwoPair; }
+            if (_containsPair) { return HandTypes.Pair; }
+
+            return HandTypes.HighCard;
+        }
     }
 }
